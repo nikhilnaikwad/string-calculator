e7461c4cb5b207c531ee33fc1f7a3cbcd35f6bac
nikhil naikwad <nikhilnaikwad71@gmail.com>
2024-09-30 16:02:07 +0530

    Changes for writing test for empty string

---
+++ calculator/src/app/services/string-calculator.service.spec.ts
@@ -0,0 +1,20 @@
+import { TestBed } from '@angular/core/testing';
+
+import { StringCalculatorService } from './string-calculator.service';
+
+describe('StringCalculatorService', () => {
+  let service: StringCalculatorService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(StringCalculatorService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should return 0 for an empty string', () => {
+    expect(service.add('')).toBe(0);
+  });
+});
